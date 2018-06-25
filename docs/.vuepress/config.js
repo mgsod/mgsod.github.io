751b68c0d99a625602dc2d571152fdbbfaf13a33
mgso <mashuai@birdcc.com>
2018-06-25 14:58:45 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by setting on 2018/6/23.
+ */
+module.exports = {
+    title: 'mgso',
+    description: 'blog',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'VuePress', link: 'https://vuepress.vuejs.org/' },
+        ]
+    }
+};
