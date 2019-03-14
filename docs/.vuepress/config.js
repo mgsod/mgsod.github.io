beadeea8ebbc49a06ff65f49c255bdfc35a6a48a
mashuai <mgso@foxmail.com>
2019-03-14 11:31:25 +0800

    修改样式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,9 +14,11 @@ module.exports = {
     themeConfig: {
         nav: [
             {text: 'Home', link: '/'},
+            {text: 'ssr', link: '/ssr/createSsr'},
+            {text: '前端', link: '/front-end/zipImg'},
             {text: 'github', link: 'https://github.com/mgsod'},
         ],
-        sidebar: [
+        /*sidebar: [
             {
                 title: '前端',
                 collapsable: false,
@@ -37,8 +39,23 @@ module.exports = {
                 ]
             }
 
-        ],
-        lastUpdated: '最后更新时间'
+        ],*/
+        sidebar: {
+            // 侧边栏在 /foo/ 上
+            '/ssr/': [
+                ['/ssr/createSsr', '搭建ssr'],
+                ['/ssr/ssrClient', 'ssr客户端配置'],
+                ['/ssr/serverSpeeder', '更换内核并安装锐速']
+            ],
+            // 侧边栏在 /bar/ 上
+            '/front-end/': [
+                ['/front-end/zipImg', 'js压缩图片(上传)'],
+                ['/front-end/reg', '正则分组(),不捕获(?:)以及断言'],
+                ['/front-end/for', 'for in 和 for of'],
+                ['/front-end/pxtorem', '移动端自适应,px转rem'],
+            ]
+        },
+        lastUpdated: 'lastUpdated'
     },
     markdown: {
         lineNumbers: true
--- docs/README.md
+++ docs/README.md
@@ -1,12 +1,19 @@
+---
+home: true
+heroImage: /hero.png
+footer: MIT Licensed | Copyright © 2019-present mgso
+---
 
-### mgso的个人文章
-
-
-
-
-
-
-
+#### 快捷导航
 
 
-##### 文章系统基于 [vresspress](https://vuepress.vuejs.org/)
+[ssr](/ssr/createSsr.html "ssr") 
+
+
+[前端](/front-end/zipImg.html "前端") 
+
+
+
+[github](https://github.com/mgsod "github/mgsod") 
+
+
