183dfe2666f2b46a3d717dcd5e3433440d56d320
mash <mgso@foxmail.com>
2019-09-12 14:58:30 +0800

    修改首页，添加组件

---
+++ docs/.vuepress/components/msProgress.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="progress">
+        <div class="name">{{name}}</div>
+        <progress max="100" :value="value || 8"></progress>
+    </div>
+</template>
+<script>
+    export default {
+        name:'msProgress',
+        props:['name','value']
+    }
+</script>
+<style lang="less">
+    .progress{
+        display: flex;
+        align-items: center;
+        margin: 10px 0;
+        .name{
+            margin-top: -5px;
+            margin-right: 10px;
+            width: 100px;
+            text-align: left;
+        }
+    }
+</style>
